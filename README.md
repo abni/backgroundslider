6e0deb3ee7ed5a8021813ca4af8c28cf0c4d6b2f
Anders Bergmann <abni@eb.dk>
2015-01-02 22:08:09 +0100

    Two way binding for index variable added. Touch events enabled.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ An Angular directive to create fullscreen background slideshows.
 
 Check out the [demo here](http://andersbergmann.dk/backgroundslider/example.html)
 
-The directive has only been tested in Chrome!
+The directive has only been tested in modern browsers and on iOS devices.
 
 # Use
 Add these files to your project (in the dist folder you will find minified versions):
@@ -12,15 +12,19 @@ dist/backgroundslider.css
 dist/backgroundslider.js
 dist/backgroundslider.html
 
-Dependencies:
+# Dependencies:
 angular
 angular-animate
+angular-touch
 
 Drop the slider element into your HTML:
 `<slider images="images" index="index"></slider>`
 
+`images` is an array of image objects.
+`index` is two-way binding (between directive and and the $scope).
+
 Tell the slider which images to use from your controller:
-`$scope.images = [{src: 'images/img.jpg', caption: 'lorem ipsom'}];
+`$scope.images = [{src: 'images/img.jpg', caption: 'lorem ipsom'}];`
 
 See example.html for more.
 
--- backgroundslider.js
+++ backgroundslider.js
@@ -4,36 +4,37 @@ app.directive('slider', function () {
         restrict: 'AE',
         replace: true,
         scope: {
-            images: '='
+            images: '=',
+            index: '='
         },
         link: function(scope){
-            scope.currentIndex = 0;
+            scope.index = 0;
             var iOS = /(iPad|iPhone|iPod|Safari)/g.test( navigator.userAgent );
 
             scope.next = function() {
-                scope.currentIndex < scope.images.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
+                scope.index < scope.images.length - 1 ? scope.index++ : scope.index = 0;
             };
 
             scope.prev = function() {
-                scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.images.length - 1;
+                scope.index > 0 ? scope.index-- : scope.index = scope.images.length - 1;
             };
 
             scope.$watch('images', function (newValue, oldValue) {
                 if( newValue !== oldValue ){
-                    scope.$watch('currentIndex', function() {
+                    scope.$watch('index', function() {
                         scope.images.forEach(function(image) {
                             image.visible = false;
                         });
-                        scope.images[scope.currentIndex].visible = true;
+                        scope.images[scope.index].visible = true;
                     });
                 }else{
                     // This is needed to make sure iOS devices load images
                     if (iOS) {
-                        scope.$watch('currentIndex', function() {
+                        scope.$watch('index', function() {
                             scope.images.forEach(function(image) {
                                 image.visible = false;
                             });
-                            scope.images[scope.currentIndex].visible = true;
+                            scope.images[scope.index].visible = true;
                         });
                     }
                 }
